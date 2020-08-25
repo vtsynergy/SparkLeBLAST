d203675cbcbcac4b08032eb29bd70333838b58d5
Karim Youssef <karimy@vt.edu>
2020-08-25 19:05:04 -0400

    Update cluster-specific params with placeholders

--- SparkLeMakeDB.sh
+++ SparkLeMakeDB.sh
@@ -3,13 +3,13 @@
 #SBATCH --time=07:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH -A hpcbigdata2
-#SBTACH -p normal_q
+#SBATCH -A <allocation_name>
+#SBTACH -p <partition_name>
 
 module load jdk
 
-masterAddress="spark://ca015.ca.arc.internal:7077"
-dbPath="/work/cascades/karimy/blastDB/nr"
+masterAddress="spark://<HOSTNAME>:7077"
+dbPath="/path/to/db"
 numPartitions=1000
-dbName="/work/cascades/karimy/blastDB/nr_1000_indexed"
-/home/karimy/spark-2.2.0-bin-hadoop2.6/bin/spark-submit --conf "spark.local.dir=/home/karimy/tmpSpark" --conf "spark.network.timeout=3600" --conf "spark.driver.extraJavaOptions=-XX:MaxHeapSize=124g" --conf "spark.worker.extraJavaOptions=-XX:MaxHeapSize=124g" --master $masterAddress --driver-memory 124g --executor-memory 124g --class MakeDB target/scala-2.11/simple-project_2.11-1.0.jar $numPartitions $dbPath "/home/karimy/SparkLeBLAST/formatdbScript" $dbName;
+dbName="$dbPath_<name>"
+<path_to_spark_bin>/spark-submit --conf "spark.local.dir=/home/karimy/tmpSpark" --conf "spark.network.timeout=3600" --conf "spark.driver.extraJavaOptions=-XX:MaxHeapSize=124g" --conf "spark.worker.extraJavaOptions=-XX:MaxHeapSize=124g" --master $masterAddress --driver-memory 124g --executor-memory 124g --class MakeDB target/scala-2.11/simple-project_2.11-1.0.jar $numPartitions $dbPath "<SparkLeBLAST_base_dir>/formatdbScript" $dbName;
