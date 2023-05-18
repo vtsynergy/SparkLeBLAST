a34a228b4d267db65e8f8b43e98423f64479f43d
Emil VATAI <emil.vatai@gmail.com>
2023-05-18 09:21:59 +0900

    Add makedb.sh

---
+++ containers/singularity/makedb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# singularity exec --no-home --bind hosts:/etc/hosts sparkleblast_latest.sif /bin/bash
+
+SINGULARITY_ARGS=(
+
+  --env SPARK_HOME=/opt/spark-2.2.0-bin-hadoop2.6
+  --env NCBI_BLAST_PATH=/opt/ncbi-blast-2.13.0+/bin
+  --env SLB_WORKDIR=/opt/sparkleblast
+  --bind hosts:/etc/hosts 
+  --bind ../../../data:/tmp/data
+)
+
+MAKEDB_ARGS=(
+  -p 2 
+  -i /tmp/data/swissprot 
+  -t /tmp/data/sharedout 
+  -m spark://$(hostname):7077
+)
+
+singularity exec ${SINGULARITY_ARGS[@]} sparkleblast_latest.sif \
+  /opt/sparkleblast/SparkLeMakeDB.sh ${MAKEDB_ARGS[@]}
