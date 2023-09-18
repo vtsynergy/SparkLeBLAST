cef38987c48e44ded8bda9de3e2e155e793bcfe5
Emil VATAI <emil.vatai@gmail.com>
2023-09-19 08:50:57 +0900

    Update fsub.sh

--- containers/singularity/fsub.sh
+++ containers/singularity/fsub.sh
@@ -3,22 +3,36 @@
 DBFILE=$1
 QUERYFILE=$2
 
-NPROC=3
-PJSUB_ARGS=(-N mkdb.tmp
-            -S -j
-            -x PJM_LLIO_GFSCACHE=/vol0004
-            -g ra000012
-            # --llio localtmp-size=10Gi
-            -L rscgrp=small
-            -L node=$NPROC
-            -L elapse=0:10:00
-            -L jobenv=singularity
-            --mpi proc=$NPROC)
+USAGE="$0 \${NPROC}"
+if [ -z ${NPROC} ] || [ ! -e "data/${DBFILE}" ] || [ ! -e "data/${QUERYFILE}" ]; then
+  echo "NPROC not set!" # TODO separate errors
+  echo ${USAGE}
+  exit 1
+fi
+if [ ${NPROC} -gt 384 ]; then 
+  RSCGRP=large;
+else
+  RSCGRP=small;
+fi
 
-rm mkdb.tmp.* output.* # must be outside pjsub
-rm -rf  run/* log/* work/* data/out/*
-rm -rf  hosts master_success
-pjsub ${PJSUB_ARGS[@]} << EOF
+PJSUB_ARGS=(
+  -N slarkle-${NPROC}
+  -S -j
+  -x PJM_LLIO_GFSCACHE=/vol0004
+  -g ra000012
+  # --llio localtmp-size=10Gi
+  -L rscgrp=${RSCGRP}
+  -L node=${NPROC}
+  -L elapse=10:00:00
+  -L jobenv=singularity
+  --mpi proc=${NPROC}
+)
+
+# rm mkdb.tmp.* output.* # must be outside pjsub
+# rm -rf  run/* log/* work/* data/out/*
+# rm -rf  hosts master_success
+echo pjsub ${PJSUB_ARGS[@]} 
+cat << EOF
 mpiexec ./gatherhosts_ips hosts
 mpiexec ./start_spark_cluster.sh &
 ./run_spark_jobs.sh ${DBFILE} ${QUERYFILE}
