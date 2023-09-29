c9c5907e4cdfec237de60b18a7e5ae2f68ab6c5e
Emil VATAI <emil.vatai@gmail.com>
2023-09-29 12:49:28 +0900

    Make interactive nicer

--- containers/singularity/get_interactive_fugaku_node.sh
+++ containers/singularity/get_interactive_fugaku_node.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-cat fsub.sh; rm -rf  hosts master_success; pjsub --interact --sparam wait-time=60 -x PJM_LLIO_GFSCACHE=/vol0004 -g ra000012 --llio localtmp-size=10Gi -L "node=2,elapse=2:00:00,jobenv=singularity" --mpi proc=2
+rm -rf  hosts master_success
+NPROC=2
+PJSUB_ARGS=(
+  -L elapse=30:00
+  -L node=${NPROC}
+  --mpi proc=${NPROC}
+  --interact
+  --sparam wait-time=60
+  -x PJM_LLIO_GFSCACHE=/vol0004
+  -g ra000012
+  --llio localtmp-size=10Gi
+  -L jobenv=singularity
+)
+pjsub  ${PJSUB_ARGS[@]}
