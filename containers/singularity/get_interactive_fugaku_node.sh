e25c0e9d1c3a893947be1daca7c01ec22e2ad12f
Emil VATAI <emil.vatai@gmail.com>
2023-11-07 11:47:03 +0900

    Update interactive node script

--- containers/singularity/get_interactive_fugaku_node.sh
+++ containers/singularity/get_interactive_fugaku_node.sh
@@ -2,14 +2,15 @@
 rm -rf  hosts master_success
 NPROC=2
 PJSUB_ARGS=(
-  -L elapse=30:00
+  -L elapse=1:30:00
   -L node=${NPROC}
   --mpi proc=${NPROC}
   --interact
   --sparam wait-time=60
   -x PJM_LLIO_GFSCACHE=/vol0004
+  -x SINGULARITY_TMPDIR=/local
   -g ra000012
-  --llio localtmp-size=10Gi
+  --llio localtmp-size=20Gi
   -L jobenv=singularity
 )
 pjsub  ${PJSUB_ARGS[@]}
