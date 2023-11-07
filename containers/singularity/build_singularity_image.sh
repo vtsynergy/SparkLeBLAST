707d3642fff6d10ef433135abcaca2a04802cf69
Emil VATAI <emil.vatai@gmail.com>
2023-11-07 11:45:26 +0900

    Update and rename init to build...sh

---
+++ containers/singularity/build_singularity_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PJM -L elapse=1:30:00
+#PJM -L node=1
+#PJM -x PJM_LLIO_GFSCACHE=/vol0004
+#PJM -x SINGULARITY_TMPDIR=/local
+#PJM -g ra000012
+#PJM --llio localtmp-size=20Gi
+#PJM -L jobenv=singularity
+
+singularity pull -F docker://karimyoussef1991/sparkleblast:latest
--- containers/singularity/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p log run work
-
-singularity pull -F docker://karimyoussef1991/sparkleblast:latest
