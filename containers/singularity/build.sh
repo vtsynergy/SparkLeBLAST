75e5df3c1fbff194b637052d33757173d4c120e9
Emil VATAI <emil.vatai@gmail.com>
2023-11-11 15:34:07 +0900

    Separate (docker)-base and sparkleblast (singularity)

--- containers/singularity/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.13.0/ncbi-blast-2.13.0+-src.tar.gz -P /opt
-docker buildx build  . --platform linux/arm64 --tag karimyoussef1991/sparkleblast:latest --push
-
--- containers/singularity/build_singularity_image.sh
+++ containers/singularity/build_singularity_image.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#PJM -L elapse=0:30:00
+#PJM -L elapse=1:00:00
 #PJM -L node=1
-#PJM -x PJM_LLIO_GFSCACHE=/vol0004
-#PJM -x SINGULARITY_TMPDIR=/local
 #PJM -g ra000012
 #PJM --llio localtmp-size=20Gi
 #PJM -L jobenv=singularity
+#PJM -x PJM_LLIO_GFSCACHE=/vol0004
+#PJM -x SINGULARITY_TMPDIR=/local
 
-singularity pull -F docker://karimyoussef1991/sparkleblast:latest
+export TMPDIR=/worktmp
+singularity build --fakeroot --no-setgroups sparkleblast_latest.sif sparkleblast_latest.def
 
 echo "DONE"
