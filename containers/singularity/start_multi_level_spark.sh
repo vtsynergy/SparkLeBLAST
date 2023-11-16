08e323040aaa6ba9452c81b375808afdcbaf324f
Emil VATAI <emil.vatai@gmail.com>
2023-11-16 14:54:30 +0900

    Copy `start_spark_cluster`

--- containers/singularity/start_multi_level_spark.sh
+++ containers/singularity/start_multi_level_spark.sh
@@ -31,6 +31,19 @@ start_master() {
 
 start_worker () {
     echo WORKER: $NPART
+    while [ ! -e "master_success" ]; do
+        sleep 1
+    done
+    master_node=$(head -n 1 master_success)
+    singularity instance start ${SINGULARITY_ARGS[@]} sparkleblast_latest.sif spark-process
+    sleep 5
+    master_url="spark://${master_node}:7077"
+    echo "Starting worker on node: $(hostname)"
+    singularity exec instance://spark-process /opt/spark-2.2.0-bin-hadoop2.6/sbin/start-slave.sh ${master_url}
+
+    while [ -e "master_success" ]; do
+        sleep 1
+    done
 }
 
 start_cluster () {
