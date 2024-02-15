1c0c223b75c9bce8bdf05824a1b52d10229a41c5
Ritvik Prabhu <ritvikp@vt.edu>
2024-02-15 16:36:40 -0500

    Created env variables in run_scaling.sh

--- run_scaling.sh
+++ run_scaling.sh
@@ -2,6 +2,12 @@
 
 # 128 nodes, preemptable_q
 export SPARK_SLURM_PATH=/home/karimy/spark-slurm/spark_normal_full_node
+export SPARK_HOME=/home/karimy/spark-2.2.0-bin-hadoop2.6 # Where pre-built Spark was downloaded from dependencies abpve 
+# export NCBI_BLAST_PATH=</path/to/ncbi_blast/binaries>
+export SLB_WORKDIR=/home/karimy/SparkLeBLAST # Path to SparkLeBLAST root directory
+export SPARK_LOCAL_DIRECTORY=/home/karimy/tmpSpark
+
+
 # ./SparkLeMakeAndSearch.sh -i /fastscratch/karimy/blastDB/nr -t /fastscratch/karimy/blastDB/nr_formatted -q /fastscratch/karimy/blastDB/GeobacterMetallireducens.fasta -o T  -dbtype prot -p 128 -w 128 -time 480 -h tc -d /fastscratch/karimy/SparkLogs
 
 # Other configs, normal_q
