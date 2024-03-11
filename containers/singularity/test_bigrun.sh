f0e2bb0c085e1a2fe766b1a2e62cab0d0b7308f3
Emil VATAI <emil.vatai@gmail.com>
2024-03-11 23:05:16 +0900

    Make it run again on fugaku

--- containers/singularity/test_bigrun.sh
+++ containers/singularity/test_bigrun.sh
@@ -1,2 +1,5 @@
+CLEARALL=no ./fsub.sh nt non-rRNA-reads.fa 16  12:00:00
+CLEARALL=no ./fsub.sh nt non-rRNA-reads.fa 32  12:00:00
+CLEARALL=no ./fsub.sh nt non-rRNA-reads.fa 64  12:00:00
 CLEARALL=no ./fsub.sh nt non-rRNA-reads.fa 128  12:00:00
 
