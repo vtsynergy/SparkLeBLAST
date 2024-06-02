c9678ac7975b0549385a06c25b0bb812e8883f4c
Ritvik Prabhu <rprabhu@login2.cm.cluster>
2024-06-02 01:39:22 -0400

    Running only 2 nodes

--- containers/singularity/test_bigrun.sh
+++ containers/singularity/test_bigrun.sh
@@ -1,8 +1,8 @@
-#CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 1 long 12:00:00 /lustre/scratch/rprabhu/data
 CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 2 long 12:00:00 /lustre/scratch/rprabhu/data
-CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 4 long 12:00:00 /lustre/scratch/rprabhu/data
-CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 8 medium 12:00:00 /lustre/scratch/rprabhu/data
-CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 16 medium 12:00:00 /lustre/scratch/rprabhu/data
-CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 32 medium 12:00:00 /lustre/scratch/rprabhu/data
+#CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 3 long 12:00:00 /lustre/scratch/rprabhu/data
+#CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 5 long 12:00:00 /lustre/scratch/rprabhu/data
+#CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 9 medium 12:00:00 /lustre/scratch/rprabhu/data
+#CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 17 medium 12:00:00 /lustre/scratch/rprabhu/data
+#CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 33 medium 12:00:00 /lustre/scratch/rprabhu/data
 #CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 64 large 08:00:00 /lustre/scratch/rprabhu/data
 #CLEARALL=no ./slurm_sub.sh nr non-rRNA-reads.fa 128 all-nodes 12:00:00 /lustre/scratch/rprabhu/data
