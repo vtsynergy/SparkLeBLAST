278270dbe557449fc3cc9d8da6a97d0d97486eeb
Ritvik Prabhu <rprabhu@login2.cm.cluster>
2024-06-04 08:04:08 -0400

    fixed file system

--- containers/singularity/build_singularity_image.sh
+++ containers/singularity/build_singularity_image.sh
@@ -4,6 +4,8 @@
 #SBATCH --time=4:00:00       
 #SBATCH --nodes=1            
 #SBATCH -p short
+
+module load gcc/13.2.0
 singularity build --remote sparkleblast_latest.sif sparkleblast_latest.def
 
 echo "DONE"
