5b1fbeadb24b9a4e04ea91efc7011895ffdf4bea
Emil VATAI <emil.vatai@gmail.com>
2024-08-03 23:16:00 +0900

    Clean up `get_interactive_fugaku_node.sh`

--- containers/singularity/get_interactive_fugaku_node.sh
+++ containers/singularity/get_interactive_fugaku_node.sh
@@ -3,8 +3,9 @@ rm -rf  hosts master_success
 NPROC=2
 PJSUB_ARGS=(
   -L elapse=1:30:00
-  -L node=${NPROC}
-  --mpi proc=${NPROC}
+  -L node="${NPROC}"
+  # -L freq=2200 -L throttling_state=0 -L issue_state=0 -L ex_pipe_state=0 -L eco_state=0 -L retention_state=0
+  --mpi proc="${NPROC}"
   --interact
   --sparam wait-time=60
   -x PJM_LLIO_GFSCACHE=/vol0004
@@ -13,4 +14,4 @@ PJSUB_ARGS=(
   --llio localtmp-size=20Gi
   -L jobenv=singularity
 )
-pjsub  ${PJSUB_ARGS[@]}
+pjsub  "${PJSUB_ARGS[@]}"
